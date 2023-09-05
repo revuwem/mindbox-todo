51d3d6eda738451d1d0f386e012d699bc1f35a6d
Karina Grishaeva <revuwem@gmail.com>
2023-09-05 23:11:00 +0400

    feat(helpers): create renderWithStore test helper

---
+++ src/helpers/tests/renderWithStore.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import type { RenderOptions } from "@testing-library/react";
+import type { PreloadedState } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+
+import { setupStore } from "@/store";
+import type { AppStore, RootState } from "@/store";
+
+// This type interface extends the default options for render from RTL, as well
+// as allows user to specify other things such as initialState, store
+interface ExtendedRenderOptions extends Omit<RenderOptions, "queries"> {
+  preloadedState?: PreloadedState<RootState>;
+  store?: AppStore;
+}
+
+export function renderWithStore(
+  ui: React.ReactElement,
+  {
+    preloadedState = {},
+    store = setupStore(preloadedState),
+    ...renderOptions
+  }: ExtendedRenderOptions = {}
+) {
+  function Wrapper({ children }: React.PropsWithChildren): JSX.Element {
+    return <Provider store={store}>{children}</Provider>;
+  }
+
+  return { store, ...render(ui, { wrapper: Wrapper, ...renderOptions }) };
+}
